44d0bca7284ccf3429ee2de3eead510a286c7755
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-13 21:22:44 +0100

    Update theme colors & tune the global layout CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,14 +11,27 @@ export const metadata: Metadata = {
     'Joue avec des chaussettes de sport personnalisées, à ton effigie et pour toutes tes compétitions !',
 };
 
+const sizeOfLogo = 52;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="" lang="en">
+      <body className={` ${inter.className}`}>
+        <header className="m-4 flex justify-center">
+          <Image
+            priority
+            src="/svgs/logo.svg"
+            height={sizeOfLogo}
+            width={sizeOfLogo}
+            alt="Marque des Frères Chaussettes"
+          />
+        </header>
+        <main className="flex-grow">{children}</main>
+      </body>
     </html>
   );
 }
