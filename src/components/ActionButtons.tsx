c0c2c1ae10a67862bbb70f1d23cef5f1a4b1b06d
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-25 20:49:40 +0100

    Refactor: move action buttons to their own component.

---
+++ src/components/ActionButtons.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+type ActionButtonsProps = Readonly<{
+  primaryButton: {
+    label: string;
+    action: () => void;
+  };
+  secondaryButton?: {
+    label: string;
+    action: () => void;
+  };
+}>;
+
+export default function ActionButtons({
+  primaryButton,
+  secondaryButton,
+}: ActionButtonsProps) {
+  return (
+    <div className="m-5 flex flex-grow flex-col justify-end gap-2">
+      {primaryButton && (
+        <Button className="text-white" onClick={primaryButton.action}>
+          {primaryButton.label}
+        </Button>
+      )}
+      {secondaryButton && (
+        <Button variant="outline" onClick={secondaryButton.action}>
+          {secondaryButton.label}
+        </Button>
+      )}
+    </div>
+  );
+}
--- src/components/checkout/CheckoutLayout.tsx
+++ src/components/checkout/CheckoutLayout.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import TypographyH1 from '../typography/TypographyH1';
 import Subtitle from '../typography/Subtitle';
+import ActionButtons from '../ActionButtons';
+
+type CheckoutButtonProps = {
+  label: string;
+  relativePathToNextPage: string;
+};
 
 type CheckoutLayoutProps = Readonly<{
   title: string;
   subtitle?: string;
-  primaryButton?: {
-    label: string;
-    relativePathToNextPage: string;
-  };
-  secondaryButton?: {
-    label: string;
-    relativePathToNextPage: string;
-  };
+  primaryButton?: CheckoutButtonProps;
+  secondaryButton?: CheckoutButtonProps;
   children: React.ReactNode;
 }>;
 
@@ -28,33 +27,24 @@ export default function CheckoutLayout({
 }: CheckoutLayoutProps) {
   const { push } = useRouter();
 
-  const primaryButtonElement = primaryButton && (
-    <Button
-      className="text-white"
-      onClick={() => push(primaryButton.relativePathToNextPage)}
-    >
-      {primaryButton.label}
-    </Button>
-  );
-
-  const secondaryButtonElement = secondaryButton && (
-    <Button
-      variant="outline"
-      onClick={() => push(secondaryButton.relativePathToNextPage)}
-    >
-      {secondaryButton.label}
-    </Button>
-  );
+  const generateButtonProps = (button: CheckoutButtonProps) => ({
+    label: button.label,
+    action: () => push(button.relativePathToNextPage),
+  });
 
   return (
     <div className="mx-5 flex h-full flex-col">
       <TypographyH1 text={title} />
       {subtitle && <Subtitle text={subtitle} />}
       <div className="relative h-full w-full">{children}</div>
-      <div className="m-5 flex flex-grow flex-col justify-end gap-2">
-        {primaryButtonElement}
-        {secondaryButtonElement}
-      </div>
+      {primaryButton && (
+        <ActionButtons
+          primaryButton={generateButtonProps(primaryButton)}
+          secondaryButton={
+            secondaryButton && generateButtonProps(secondaryButton)
+          }
+        />
+      )}
     </div>
   );
 }
