2b77f451b2740594c7b9eee17c454439b7612158
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-27 20:02:24 +0100

    Redirect the root URL to the home page of Les Frères Chaussettes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <div className="flex h-full flex-col items-center justify-center">
-      Landing screen
-    </div>
-  );
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+    redirect('https://www.lesfrereschaussettes.com/');
 }
