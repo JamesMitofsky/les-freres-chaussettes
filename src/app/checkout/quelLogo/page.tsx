cfc8a5f30ceb4cc8db054529abc81a12e9a320e0
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-27 20:17:20 +0100

    Feat: basic design for logo upload.

--- src/app/checkout/quelLogo/page.tsx
+++ src/app/checkout/quelLogo/page.tsx
@@ -1,12 +1,25 @@
 import CheckoutWrapper from '@/components/checkout/CheckoutWrapper';
+import Image from 'next/image';
 
 export default function SelectLogo() {
   return (
     <CheckoutWrapper
       title="Quel logo souhaitez-vous utiliser ?"
-      primaryButton={{ label: 'Continuer', relativePathToNextPage: 'faceArriere'}}
-     >
-      customized content for logo selection
+      primaryButton={{
+        label: 'Continuer',
+        relativePathToNextPage: 'faceArriere',
+      }}
+    >
+      <div className="flex flex-col gap-2 h-48 w-full items-center justify-center rounded-lg border-2 border-dotted border-primary">
+        <Image
+          priority
+          src="/svgs/upload-arrow.svg"
+          height={80}
+          width={80}
+          alt="Uploader mon logo"
+        />
+        Utiliser mon logo
+      </div>
     </CheckoutWrapper>
   );
 }
