0b91dafe2ac6cbfaf57be01b4c7e17d4c4bd0c3f
James Mitofsky <JamesMitofsky@gmail.com>
2024-03-09 17:09:24 +0100

    Refine POC of admin landing page.

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export default function Login() {
   return (
-    <form action="/api/machin">
-      <input type="text" placeholder="Mot de passe" />
-      <button type="submit">Connexion</button>
-    </form>
+    <div className="m-auto flex h-full w-full max-w-sm flex-col items-start justify-center gap-10 p-10">
+      <div>
+        <h1 className="text-2xl font-semibold tracking-tight">Accès Admin</h1>
+        <p className="text-sm text-muted-foreground">
+          Enter le mot de passe pour accéder à l'interface d'administration.
+        </p>
+      </div>
+      <Input />
+      <Button
+        onClick={() => console.log('Sign in clicked')}
+        variant="gooeyRight"
+      >
+        Connecter
+      </Button>
+    </div>
   );
 }
