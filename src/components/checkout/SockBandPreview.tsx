49594a8919becd9a3e2b5dae87546e21b483fe7d
James Mitofsky <JamesMitofsky@gmail.com>
2024-03-18 20:11:08 +0100

    Fix: alignment of the name and number on the sock band.

--- src/components/checkout/SockBandPreview.tsx
+++ src/components/checkout/SockBandPreview.tsx
@@ -1,27 +1,15 @@
-import PlayerInfo from "./PlayerInfo";
+import PlayerInfo from './PlayerInfo';
 
 type SockBandPreviewProps = { color: string | undefined };
 
-export default function SockBandPreview({ color }: SockBandPreviewProps) {    
+export default function SockBandPreview({ color }: SockBandPreviewProps) {
   return (
-    <div className="flex flex-col w-min">
-      <div
-        className="mt-4 h-16 w-16"
-        style={{ backgroundColor: color }}
-      />
-      <div
-        className="mt-4 h-16 w-16"
-        style={{ backgroundColor: color }}
-      />
+    <div className="flex w-min flex-col gap-4">
+      <div className="h-16 w-16" style={{ backgroundColor: color }} />
+      <div className="h-16 w-16" style={{ backgroundColor: color }} />
       <PlayerInfo />
-      <div
-        className="mt-4 h-8 w-16"
-        style={{ backgroundColor: color }}
-      />
-      <div
-        className="mt-4 h-6 w-16"
-        style={{ backgroundColor: color }}
-      />
+      <div className="h-8 w-16" style={{ backgroundColor: color }} />
+      <div className="h-6 w-16" style={{ backgroundColor: color }} />
     </div>
   );
 }
